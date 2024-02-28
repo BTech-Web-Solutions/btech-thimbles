5c03b255f5022b3b85765195da3882b4cb4652e1
brodrigues0ll <bernardorg99@hotmail.com>
2024-02-28 15:16:37 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,128 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect, useState } from "react";
+import img0 from "../../public/assets/img0.jpg";
+import img1 from "../../public/assets/img1.jpg";
+import img2 from "../../public/assets/img2.jpg";
+import img3 from "../../public/assets/img3.jpg";
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [signal, setSignal] = useState("---");
+  const [counter, setCounter] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const [randomImage, setRandomImage] = useState(img0.src); // State to hold the random image
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const beforeRun = (e) => {
+    setRandomImage(img0.src); // Reset randomImage state to img0
+    setSignal("---"); // Reset signal state
+    setIsAnalyzing(true);
+    setTimeout(() => {
+      e.target.classList.remove("bg-[#ff0000]");
+    }, 1000);
+
+    setTimeout(() => {
+      setIsAnalyzing(false);
+      setFinished(true); // Set finished to true to trigger image change
+    }, 2000);
+
+    if (isRunning === true) {
+      e.target.classList.add("bg-[#af7d20]");
+    }
+  };
+
+  const randomImg = () => {
+    const imgs = [img1, img2, img3];
+    const random = Math.floor(Math.random() * imgs.length);
+    return imgs[random].src;
+  };
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setCounter((prev) => prev - 1);
+      }, 1000);
+
+      if (counter === 0) {
+        setIsRunning(false);
+      }
+
+      return () => clearInterval(interval);
+    }
+  }, [counter, isRunning]);
+
+  useEffect(() => {
+    const btn = document.querySelector("button");
+
+    if (isAnalyzing) {
+      btn.classList.add("bg-[#ff0000]");
+      btn.innerHTML = "Analisando...";
+
+      setTimeout(() => {
+        btn.classList.remove("bg-[#ff0000]");
+        btn.classList.add("bg-[#f59e07]");
+        btn.innerHTML = "Sinal Encontrado!";
+      }, 1000);
+    }
+  }, [isAnalyzing]);
+
+  // Update random image when finished changes
+  useEffect(() => {
+    if (finished) {
+      const random = randomImg();
+      setRandomImage(random); // Update randomImage state with the new random image
+
+      switch (random) {
+        case img1.src:
+          setSignal("Primeiro Copo");
+          break;
+        case img2.src:
+          setSignal("Segundo Copo");
+          break;
+        case img3.src:
+          setSignal("Terceiro Copo");
+          break;
+        default:
+          setSignal("---");
+          break;
+      }
+
+      setCounter(10); // Reset counter to 10
+      setIsRunning(true); // Start the countdown
+      setFinished(false); // Reset finished state
+    }
+  }, [finished]);
+
+  return (
+    <main className="mx-auto">
+      <div className="p-5 text-5xl flex flex-col items-center gap-5">
+        <h1>Thimbles</h1>
+        <img
+          src={randomImage} // Use randomImage state here
+          alt="Thimbles"
+          className="h-[350px] rounded-xl"
+        />
+
+        <div className="bg-[#2e1708] w-[100%] flex flex-col items-center rounded-xl p-2 shadow-lg">
+          <h1 className="text-[#FF5733] font-medium text-sm">SINAL</h1>
+          <h2 className="text-white font-medium text-2xl">{signal}</h2>
+        </div>
+
+        <button
+          className={`${
+            isRunning === false ? "bg-[#309009]" : "bg-[#af7d20] text-zinc-400"
+          } w-[100%] p-2 text-2xl font-medium rounded-xl flex justify-center items-center gap-2 cursor-pointer`}
+          onClick={beforeRun}
+        >
+          {isRunning
+            ? `${counter} ${counter === 1 ? "segundo" : "segundos"}`
+            : "Gerar Sinal"}
+        </button>
+        <iframe
+          id="frame"
+          src="https://saturnobet.com/game/evoplay-thimbles?r=AFILIADO-CODIGO"
+          className="iframe-perestop"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
